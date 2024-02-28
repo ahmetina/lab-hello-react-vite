bb17b543038edfd6850f7d9b2c183b0dfebd669c
Abraham Agusti <abraham.agusti@gmail.com>
2024-02-28 17:47:42 +0100

    Main base

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import WhiteButton from './components/WhiteButton'
+
+function App() {
+ 
+  return (
+    
+      <div className ="App">
+
+        <h1>Say hello to ReactJS</h1>
+        <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+        <WhiteButton></WhiteButton>
+
+      </div>
+    
+  )
+}
+
+export default App
